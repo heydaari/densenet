94335089c8801ef7d3a3aa8ce187e2fcc5f9cdcd
Mohammad Hassan <heidary0081@gmail.com>
2024-04-11 20:01:50 +0330

    Update DenseNet.py

--- DenseNet.py
+++ DenseNet.py
@@ -6,19 +6,21 @@ def densenet_block (x0):
     x1 = Conv2D(32, (3, 3), activation = 'relu', padding='same')(x0)
     x1 = BatchNormalization()(x1)
 
-    x_x0 = Concatenate(axis=-1)([x, x0])
-    x1 = Conv2D(32, (3, 3), activation = 'relu', padding='same')(x_x0)
-    x1 = BatchNormalization()(x1)
-
-    x_x0_x1 = Concatenate(axis=-1)([x, x0, x1])
-    x2 = Conv2D(32, (3, 3), activation='relu', padding='same')(x_x0_x1)
+    x0_x1 = Concatenate(axis=-1)([x0, x1])
+    x2 = Conv2D(32, (3, 3), activation = 'relu', padding='same')(x0_x1)
     x2 = BatchNormalization()(x2)
 
-    x_x0_x1_x2 = Concatenate(axis= -1)([x, x0, x1, x2])
-    x3 = Conv2D(32, (3, 3), activation = 'relu', padding='same')(x_x0_x1_x2)
+    x0_x1_x2 = Concatenate(axis=-1)([x0, x1, x2])
+    x3 = Conv2D(32, (3, 3), activation='relu', padding='same')(x0_x1_x2)
     x3 = BatchNormalization()(x3)
 
-    x_x0_x1_x2_x3 = Concatenate(axis = -1)([x, x0, x1, x2, ])
+    x0_x1_x2_x3 = Concatenate(axis= -1)([x0, x1, x2, x3])
+    x4 = Conv2D(32, (3, 3), activation = 'relu', padding='same')(x0_x1_x2_x3)
+    x4 = BatchNormalization()(x4)
+
+    transotion = x4
+
+    return transotion
 
 
 
