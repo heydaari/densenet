f2cdc542a0876627ce3da6076c015a9e215f213c
Mohammad Hassan <heidary0081@gmail.com>
2024-04-13 11:42:51 +0330

    update dense block

--- DenseNet.py
+++ DenseNet.py
@@ -2,26 +2,19 @@ import tensorflow as tf
 from keras.layers import Dense, Dropout, BatchNormalization, ReLU, Conv2D, Concatenate
 
 
-def densenet_block (x0):
-    x1 = Conv2D(32, (3, 3), activation = 'relu', padding='same')(x0)
-    x1 = BatchNormalization()(x1)
+def densenet_block (input_layer, num_convs):
 
-    x0_x1 = Concatenate(axis=-1)([x0, x1])
-    x2 = Conv2D(32, (3, 3), activation = 'relu', padding='same')(x0_x1)
-    x2 = BatchNormalization()(x2)
+    merge = []
+    merge.append(input_layer)
+    for i in range(num_convs):
+        x = Conv2D(32, (3, 3), padding='same')(input_layer)
+        x = ReLU()(x)
+        x = BatchNormalization()(x)
 
-    x0_x1_x2 = Concatenate(axis=-1)([x0, x1, x2])
-    x3 = Conv2D(32, (3, 3), activation='relu', padding='same')(x0_x1_x2)
-    x3 = BatchNormalization()(x3)
+        merge.append(x)
+        x = Concatenate(axis= -1)(merge)
 
-    x0_x1_x2_x3 = Concatenate(axis= -1)([x0, x1, x2, x3])
-    x4 = Conv2D(32, (3, 3), activation = 'relu', padding='same')(x0_x1_x2_x3)
-    x4 = BatchNormalization()(x4)
-
-    transotion = x4
-
-    return transotion
-
+    return
 
 
 
